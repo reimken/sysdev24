f79e75ab186bc51e78a0b504861fc2a3e0af9a77
Barócsi Benjámin <beno922@gmail.com>
2024-05-02 19:19:55 +0200

    Modellek átalakítva, Controllerben API-k felépítve

---
+++ Redmine/Redmine/Redmine/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Redmine.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int TypeId { get; set; }
+        public string Description { get; set; }
+        public List<int> DeveloperIds { get; set; } // A projekthez hozzárendelt fejlesztők azonosítói
+    }
+}
